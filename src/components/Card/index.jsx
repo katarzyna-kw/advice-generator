d51057fd7999c0849a3f8e9d1fa9849bd38c80c3
katarzyna-kw <katarzyna.koch@gmail.com>
2022-03-27 10:09:11 -0500

    initial api call and styling tweaks

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,10 +2,14 @@ import React, { useState, useEffect } from 'react'
 import  dividerDesktop from "../../images/pattern-divider-desktop.svg"
 import dividerMobile from "../../images/pattern-divider-mobile.svg"
 import dice from "../../images/icon-dice.svg"
+import './Card.css'
 
 function Card() {
 
   const [isMobile, setIsMobile] = useState(window.innerWidth < 550);
+  const [error, setError] = useState(null);
+  const [advice, setAdvice] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   const updateDivider = () => {
     setIsMobile(window.innerWidth < 550);
@@ -16,14 +20,30 @@ function Card() {
     return () => window.removeEventListener("resize", updateDivider);
   });
 
+  useEffect(() => {
+    fetch("https://api.adviceslip.com/advice")
+    .then(response => response.json())
+    .then((result) => {
+      console.log("result: ", result.slip)
+      setIsLoaded(true);
+      setAdvice(result.slip)
+    },
+    (error) => {
+      console.log("ERROR")
+      }
+    )
+  }, [])
+
   return (
     <div className="card--container">
       <div className='card'>
       <header>
-        <h1 className="card__header">Advice #</h1>
+        <h1 className="card__header">
+          Advice #{advice.id}
+        </h1>
       </header>
       <h2 className="card__body">
-        "Advice"
+        "{advice.advice}"
       </h2>
       <div className="divider">
         {isMobile && <img src={dividerMobile} alt="divider" />}
